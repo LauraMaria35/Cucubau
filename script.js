3eaf7e39ddb1e87f89b9890f67f3ccd11999ad53
Laura <laura.ciocalau@gmail.com>
2023-04-30 00:14:27 +0200

    m

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var element = document.querySelector(".door");
 element.addEventListener("click", toggleDoor);
 let music1 = new Audio('Cucubau.wav');
 
+
 function toggleDoor() {
   element.classList.toggle("doorOpen");
   music1.play();
   music1.loop = true;
 }
+
